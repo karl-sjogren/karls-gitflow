5b61841ea157103efdef2fd0fd758392de43b27f
Karl-Johan Sjögren <karl.sjogren@gmail.com>
2026-01-03 22:41:37 +0100

    Updated build script and added script to install locally built tool

---
+++ local-install.sh
@@ -0,0 +1,2 @@
+dotnet pack -p:ToolCommandName=git-flow2 --output ./local-artifacts --configuration Release ./src/Karls.Gitflow.Tool/Karls.Gitflow.Tool.csproj
+dotnet tool install --global --add-source ./local-artifacts Karls.Gitflow.Tool
